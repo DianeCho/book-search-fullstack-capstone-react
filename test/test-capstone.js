e501b81a5e2e29a63e3e079dc9ea286a29ef2fea
Diane Cho <diane.kim.cho@gmail.com>
2018-04-19 21:22:30 -0500

    update test for each component

--- test/test-capstone.js
+++ test/test-capstone.js
@@ -1,67 +1,46 @@
-const {
-    app,
-    runServer,
-    closeServer
-} = require('../server');
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
 
+import MainPage from '../js/components/main-page';
+import LoginPage from '../js/components/login-page';
+import SearchPage from '../js/components/search-page';
+import BooksPage from '../js/components/books-page';
+import FooterPage from '../js/components/footer-page';
+import NewuserPage from '../js/components/newuser-page';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-
-
-var recipe = require('../models/recipe.js');
-var list = require('../models/list.js');
-var should = chai.should();
-
-chai.use(chaiHttp);
-describe('shopping-list-node-capstone', function () {
-    //    it('Should ', function () {});
-    it('should add a recipe on POST', function (done) {
-        chai.request(app)
-            .post('/add-recipe-db/')
-            .send({
-                'name': 'Homemade Fish Balls with Spicy Fish Ball Sauce',
-                'rating': '4',
-                'course': 'main',
-                'id': 'Homemade-Fish-Balls-with-Spicy-Fish-Ball-Sauce-2272743',
-                'shortList': '[\"1 pound fish flesh\",\"2 tablespoons cornstarch\",\"2 teaspoons salt\",\"1 teaspoon sugar\",\"¼ cup cold water\",\"2 cups water\",\"⅓ cup soy sauce\",\"1 head garlic, peeled and minced\",\"3 shallots, peeled and finely chopped\",\"4 to 5Thai chili peppers, stemmed and chopped\",\"1 cup brown sugar\",\"1 tablespoon flour\",\"1 tablespoon cornstarch\",\"1 teaspoon salt or to taste\",\"½ teaspoon pepper\",\"oil\"]',
-
-            })
-            .end(function (err, res) {
-                //should.equal(err, null);
-                res.should.have.status(201);
-
-                done();
-            });
+describe('<LoginPage />', () => {
+    it('Renders without crashing', () => {
+        shallow(<LoginPage />);
     });
-    it('Should Delete an ingredient', function () {
-        chai.request(app)
-            .delete('/deletering/')
-            .then(function (res) {
-                res.should.have.status(201);
-            })
+});
+
+describe('<LoginPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
     });
-
-    it('Should Delete all recipes', function () {
-
-        chai.request(app)
-            .delete('/deleterec/')
-            .then(function (res) {
-                res.should.have.status(201);
-            })
-
-    });
-    it('Should Delete all ingredients', function () {
-        const killmsg = {
-            id: 'killAll'
-        };
-
-        chai.request(app)
-            .delete('/deletering/')
-            .then(function (res) {
-                res.should.have.status(201);
-            })
-
-    });
-
+});
+describe('<MainPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
+        });
+});
+describe('<SearchPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
+        });
+});
+describe('<BooksPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
+        });
+});
+describe('<FooterPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
+        });
+});
+describe('<NewuserPage />', () => {
+        it('Renders without crashing', () => {
+            shallow(<LoginPage />);
+        });
 });
